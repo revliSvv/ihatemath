41db5d829b440b046161a352b7a92c4567c941a7
revliSvv <87725717+revliSvv@users.noreply.github.com>
2021-10-13 18:34:25 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function findC() {
+  let input = {
+    a: prompt('In a² + b² = c², enter in "a."'),
+    b: prompt('In a² + b² = c², enter in "b."')
+  };
+
+  let aPrime = (input.a * 1) * (input.a * 1);
+  console.log(aPrime);
+
+  let bPrime = (input.b * 1) * (input.b * 1);
+  console.log(bPrime);
+
+  let tempC = aPrime + bPrime;
+  console.log(tempC);
+  let c = Math.sqrt(tempC);
+
+  console.log(c);
+  alert(c);
+}
